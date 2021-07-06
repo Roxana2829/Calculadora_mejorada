29134864c956b2ea4e1f18965ce904de6518159b
Roxana <arenasroxana96@gmail.com>
2021-07-06 00:29:59 -0500

    app calculadora en función

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,80 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var operacion = ""
+    var operacionPendiente = false
+
+    lateinit var btn1:Button
+    lateinit var btn2:Button
+    lateinit var btn3:Button
+    lateinit var btn4:Button
+    lateinit var btn5:Button
+    lateinit var btn6:Button
+    lateinit var btn7:Button
+    lateinit var btn8:Button
+    lateinit var btn9:Button
+    lateinit var btn0:Button
+    lateinit var btnPor:Button
+    lateinit var btnMas:Button
+    lateinit var btnMenos:Button
+    lateinit var btnEntre:Button
+    lateinit var btnIgual:Button
+    lateinit var etVisor:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
     }
+
+    private fun initUI() {
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+        btn0 = findViewById(R.id.btn0)
+        btnPor = findViewById(R.id.btnPor)
+        btnMas = findViewById(R.id.btnMas)
+        btnMenos = findViewById(R.id.btnMenos)
+        btnEntre = findViewById(R.id.btnEntre)
+        btnIgual = findViewById(R.id.btnIgual)
+        etVisor = findViewById(R.id.etVisor)
+
+
+        btn1.setOnClickListener(this)
+        btn2.setOnClickListener(this)
+        btn3.setOnClickListener(this)
+        btn4.setOnClickListener(this)
+        btn5.setOnClickListener(this)
+        btn6.setOnClickListener(this)
+        btn7.setOnClickListener(this)
+        btn8.setOnClickListener(this)
+        btn9.setOnClickListener(this)
+        btn0.setOnClickListener(this)
+        btnPor.setOnClickListener(this)
+        btnMas.setOnClickListener(this)
+        btnMenos.setOnClickListener(this)
+        btnEntre.setOnClickListener(this)
+        btnIgual.setOnClickListener(this)
+    }
+
+    override  fun  onClick(v: View){
+        when(v!!.id){
+            R.id.btn1 ->{
+                etVisor.append("1")
+            }
+        }
+    }
+
 }
